f64106d27726110cc40088740b0bed5b30cd8b2a
scor728 <scor728@aucklanduni.ac.nz>
2024-10-20 16:33:19 +1300

    refactor: t1 code cleanup

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from collections import Counter, defaultdict
 import random
 import hashlib
-import seaborn as sns
-import numpy as np
 
-# Step 1: Load the dataset
+# Task 0 - Dataset Preprocessing
+# Load the dataset
 df = pd.read_csv('arxiv.txt', sep='\t', header=None, names=['article_id', 'words', 'date'])
 
-# Step 2: Use a Counter to count word frequencies
+# Use a counter to store the frequency of each word (This structure will be more useful later)
 word_counter = Counter()
 
-# Step 3: Iterate through each row, split words, and update the counter
+# Load the counter with the dataset words
 for words in df['words']:
-    word_list = words.split()  # Split each article's words by space
+    word_list = words.split()  # Split by space
     word_counter.update(word_list)
 
-# Step 4a: Compute the average frequency of the words in the stream.
-total_word_count = sum(word_counter.values())  # Total number of words
-unique_word_count = len(word_counter)  # Number of unique words
-average_frequency = total_word_count / unique_word_count
+# Task 1 - Brute Force Frequency Analysis
+# 1a
+word_count = sum(word_counter.values()) 
+unique_word_count = len(word_counter)
+average_frequency = word_count / unique_word_count
 
-print(f"Total word count: {total_word_count}")
-print(f"Number of unique words: {unique_word_count}")
-print(f"Average frequency of words: {average_frequency}")
+print("Word Count:",  word_count)
+print(f"Unique words:", unique_word_count)
+print(f"Average word frequency:", average_frequency)
 
-# Step 4b: Compute true frequencies and plot the curve
-# Sort words by frequency in descending order
-sorted_word_counts = sorted(word_counter.values(), reverse=True)
+# 1b
+sorted_word_count = sorted(word_counter.values(), reverse=True) # Descending order
 
-# Plotting frequencies
-plt.figure(figsize=(10, 6))
-plt.plot(sorted_word_counts)
+figure_size = (10, 6)
+
+# Plot True Word Frequencies
+plt.figure(figsize=figure_size)
+plt.plot(sorted_word_count)
 plt.xlabel('Words (ranked by frequency)')
 plt.ylabel('Frequency')
-plt.title('Word Frequency Distribution')
+plt.title('True Word Frequency Distribution')
 plt.grid(True)
 plt.show()
 
